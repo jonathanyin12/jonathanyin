5a7ceca723e8e0efcc00c442d9e15cc4322e7aba
Jonathan <jonathanyin.12@gmail.com>
2023-11-28 17:29:20 -0500

    update desc, remove resume

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -7,11 +7,11 @@ export default function About() {
       <img src="headshot.jpeg" alt="me" className={styles.headshot} />
       <h1 className={styles.author}> Jonathan Yin </h1>
       <div className={styles.desc}>
-        {`I'm a senior at Yale studying Computer Science and Statistics. My
-        interests broadly lie at the intersection of machine learning, data
-        science, and software development.`}
+        {`My interests lie at the intersection of AI, data
+        science, and software development. Previously, I studied computer science and statistics at Yale. 
+        Currently building something new.`}
       </div>
-      <div style={{ marginTop: "15px" }}>
+      {/* <div style={{ marginTop: "15px" }}>
         <a
           id="cv"
           className={styles.btn}
@@ -21,7 +21,7 @@ export default function About() {
         >
           Résumé
         </a>
-      </div>
+      </div> */}
       <Social />
     </div>
   );
