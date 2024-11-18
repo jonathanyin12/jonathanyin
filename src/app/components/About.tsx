eff3d0b8e04e99986ab876a2fe1df7c9bf88fe65
Jonathan <jonathanyin.12@gmail.com>
2024-11-18 14:21:24 -0800

    update website

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,18 +1,17 @@
+import Link from "next/link";
 import styles from "../page.module.css";
 import Social from "./Social";
 
 export default function About() {
   return (
     <div className={styles.about}>
-      <img src="headshot.jpeg" alt="me" className={styles.headshot} />
+      <img src="headshot.jpg" alt="me" className={styles.headshot} />
       <h1 className={styles.author}> Jonathan Yin </h1>
-      <div className={styles.desc}>
-        {`My interests lie at the intersection of AI, data
-        science, and software development. Previously, I studied computer science and statistics at Yale. 
-        Currently building something new.`}
-      </div>
-      {/* <div style={{ marginTop: "15px" }}>
-        <a
+      {/* <div
+        className={styles.desc}
+      >{`Passionate about AI and software | Prev. CS + Stats @ Yale`}</div> */}
+      <div style={{ marginTop: "15px" }}>
+        <Link
           id="cv"
           className={styles.btn}
           href="Jonathan_Yin_Resume.pdf"
@@ -20,8 +19,8 @@ export default function About() {
           rel="noopener noreferrer"
         >
           Résumé
-        </a>
-      </div> */}
+        </Link>
+      </div>
       <Social />
     </div>
   );
